71e2b81f51b7e1831fe61ba0075c6b8cd5e5a007
Byunk <kyungho.byoun@gmail.com>
2026-03-19 11:51:58 +0900

    feat: initial commit for tl;dm

---
+++ tldm/cli.py
@@ -0,0 +1,10 @@
+import argparse
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(prog="tldm", description="Too Long; Did Meet")
+    parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
